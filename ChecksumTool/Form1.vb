cf8a5111b6815031be44fc6acbf5061582b189d3
Teath <admin@maniagaming.net>
2013-12-27 14:02:56 +0200

    First commit

---
+++ ChecksumTool/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If File.Exists(TextBox2.Text) Then
+            Dim s512Arr = GetSHA512ForFile()
+            Dim s256Arr = GetSHA256ForFile()
+            Dim s1Arr = GetSHA1ForFile()
+            TextBox1.Text = BitConverter.ToString(s512Arr).Replace("-", "")
+            TextBox3.Text = Convert.ToBase64String(s512Arr)
+            TextBox4.Text = Convert.ToBase64String(s256Arr)
+            TextBox5.Text = BitConverter.ToString(s256Arr).Replace("-", "")
+            TextBox6.Text = Convert.ToBase64String(s1Arr)
+            TextBox7.Text = BitConverter.ToString(s1Arr).Replace("-", "")
+        Else
+            MsgBox("File does not exist!")
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Function GetSHA512ForFile() As Byte()
+        Dim fileName = TextBox2.Text
+        Dim fileBytes As Byte() = File.ReadAllBytes(fileName)
+        Dim shaM As New SHA512Managed()
+        Return shaM.ComputeHash(fileBytes)
+    End Function
+
+    Private Function GetSHA256ForFile() As Byte()
+        Dim fileName = TextBox2.Text
+        Dim fileBytes As Byte() = File.ReadAllBytes(fileName)
+        Dim shaM As New SHA256Managed()
+        Return shaM.ComputeHash(fileBytes)
+    End Function
+
+    Private Function GetSHA1ForFile() As Byte()
+        Dim fileName = TextBox2.Text
+        Dim fileBytes As Byte() = File.ReadAllBytes(fileName)
+        Dim shaM As New SHA1Managed()
+        Return shaM.ComputeHash(fileBytes)
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FileDial.ShowDialog()
+    End Sub
+
+    Private Sub FileDial_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles FileDial.FileOk
+        TextBox2.Text = FileDial.FileName
+        Button1.Enabled = True
+    End Sub
+End Class
+
---
+++ ChecksumTool/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Checksum")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Checksum")> 
+<Assembly: AssemblyCopyright("Copyright © Kristo Lippur 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d429c50d-efab-4acc-bd73-947ff230e437")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
